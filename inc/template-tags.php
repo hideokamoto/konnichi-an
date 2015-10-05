fb2a7734e321dec441a4127ac8a94c85dca6f540
Hidetaka Okamoto <hide.okamoto@digitalcube.jp>
2015-10-06 02:07:55 +0900

    FIX ISSUE
    Please remove the function the_posts_navigation(). Bckward compatibility for more than last 2 major versions are not needed.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -7,35 +7,6 @@
  * @package Konnichi An
  */
 
-if ( ! function_exists( 'the_posts_navigation' ) ) :
-/**
- * Display navigation to next/previous set of posts when applicable.
- *
- * @todo Remove this function when WordPress 4.3 is released.
- */
-function the_posts_navigation() {
-	// Don't print empty markup if there's only one page.
-	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
-		return;
-	}
-	?>
-	<nav class="navigation posts-navigation" role="navigation">
-		<h2 class="screen-reader-text"><?php esc_html_e( 'Posts navigation', 'konnichi-an' ); ?></h2>
-		<div class="nav-links">
-
-			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( esc_html__( 'Older posts', 'konnichi-an' ) ); ?></div>
-			<?php endif; ?>
-
-			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( esc_html__( 'Newer posts', 'konnichi-an' ) ); ?></div>
-			<?php endif; ?>
-
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
-}
-endif;
 
 if ( ! function_exists( 'the_post_navigation' ) ) :
 /**
