fc259d5e13642190ae56677b72b9b589a13cff9a
Hidetaka Okamoto <hide.okamoto@digitalcube.jp>
2015-10-06 02:08:40 +0900

    FIX ISSUE
    Please konnichi_an_render_title() as it is not required after WordPress release of 4.3. - extras.php

--- inc/extras.php
+++ inc/extras.php
@@ -65,17 +65,4 @@ if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
 		return $title;
 	}
 	add_filter( 'wp_title', 'konnichi_an_wp_title', 10, 2 );
-
-	/**
-	 * Title shim for sites older than WordPress 4.1.
-	 *
-	 * @link https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
-	 * @todo Remove this function when WordPress 4.3 is released.
-	 */
-	function konnichi_an_render_title() {
-		?>
-		<title><?php wp_title( '|', true, 'right' ); ?></title>
-		<?php
-	}
-	add_action( 'wp_head', 'konnichi_an_render_title' );
 endif;
